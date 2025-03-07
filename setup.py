4324f0f5677707d2e9cc4ad125a977235dae71b0
albertus <albertus@psybergate.co.za>
2025-03-07 21:45:22 +0200

    initial commit

--- setup.py
+++ setup.py
@@ -1,30 +1,63 @@
+plugin_identifier = "filamentdryer"
+
+plugin_package = "octoprint_filamentdryer"
+
+plugin_name = "Filament Dryer Controller"
+
+plugin_version = "0.1.0"
+
+plugin_description = """A plugin to monitor and control temperature using a DHT22 sensor and GPIO"""
+
+plugin_author = "naughty00shortie"
+
+plugin_author_email = "naughty00shortie@gmail.com"
+
+plugin_url = "https://github.com/naughty00shortie/octoprint-filament-dryer-plugin"
+
+plugin_license = "GPL-3.0"
+
+plugin_requires = [
+    "OctoPrint",
+    "lgpio",
+    "adafruit-circuitpython-dht"
+]
+
+plugin_additional_data = []
+
+plugin_addtional_packages = []
+
+plugin_ignored_packages = []
+
+additional_setup_parameters = {}
+
 from setuptools import setup
 
-PLUGIN_IDENTIFIER = "filamentdryer"
-PLUGIN_PACKAGE = "octoprint_filamentdryer"
-PLUGIN_NAME = "OctoPrint-FilamentTemperatureController"
-PLUGIN_VERSION = "0.1.0"
-PLUGIN_DESCRIPTION = "A plugin to monitor and control temperature using a DHT22 sensor and GPIO"
-PLUGIN_AUTHOR = "Albertus"
-PLUGIN_AUTHOR_EMAIL = "naughty00shortie@gmail.com"
-PLUGIN_URL = "https://github.com/naughty00shortie/octoprint-filament-dryer-plugin"
+try:
+    import octoprint_setuptools
+except:
+    print("Could not import OctoPrint's setuptools, are you sure you are running that under "
+          "the same python installation that OctoPrint is installed under?")
+    import sys
+    sys.exit(-1)
 
-setup(
-    name=PLUGIN_NAME,
-    version=PLUGIN_VERSION,
-    description=PLUGIN_DESCRIPTION,
-    author=PLUGIN_AUTHOR,
-    author_email=PLUGIN_AUTHOR_EMAIL,
-    url=PLUGIN_URL,
-    packages=[PLUGIN_PACKAGE],
-    install_requires=[
-        "OctoPrint",
-        "lgpio",
-        "adafruit-circuitpython-dht"
-    ],
-    entry_points={
-        "octoprint.plugin": [
-            f"{PLUGIN_IDENTIFIER} = {PLUGIN_PACKAGE}"
-        ]
-    },
+setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
+    identifier=plugin_identifier,
+    package=plugin_package,
+    name=plugin_name,
+    version=plugin_version,
+    description=plugin_description,
+    author=plugin_author,
+    mail=plugin_author_email,
+    url=plugin_url,
+    license=plugin_license,
+    requires=plugin_requires,
+    additional_packages=plugin_addtional_packages,
+    ignored_packages=plugin_ignored_packages,
+    additional_data=plugin_additional_data
 )
+
+if len(additional_setup_parameters):
+    from octoprint.util import dict_merge
+    setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
+
+setup(**setup_parameters)
