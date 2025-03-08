e9cd734a08519aa3e1e65381af4b5add44cec440
albertus <albertus@psybergate.co.za>
2025-03-08 10:52:55 +0200

    yeet

---
+++ octoprint_filamentdryer/templates/static/js/filamentdryer.js
@@ -0,0 +1,38 @@
+$(function() {
+    function FilamentDryerViewModel(parameters) {
+        var self = this;
+        self.settingsViewModel = parameters[0];
+
+        self.dryerOn = ko.observable(false);
+
+        self.toggleDryer = function() {
+            if (self.dryerOn()) {
+                $.ajax({
+                    url: API_BASEURL + "plugin/filamentdryer",
+                    type: "POST",
+                    data: JSON.stringify({ command: "stop" }),
+                    contentType: "application/json",
+                    success: function() {
+                        self.dryerOn(false);
+                    }
+                });
+            } else {
+                $.ajax({
+                    url: API_BASEURL + "plugin/filamentdryer",
+                    type: "POST",
+                    data: JSON.stringify({ command: "start" }),
+                    contentType: "application/json",
+                    success: function() {
+                        self.dryerOn(true);
+                    }
+                });
+            }
+        };
+    }
+
+    OCTOPRINT_VIEWMODELS.push({
+        construct: FilamentDryerViewModel,
+        dependencies: ["settingsViewModel"],
+        elements: ["#filamentdryer-navbar"]
+    });
+});
