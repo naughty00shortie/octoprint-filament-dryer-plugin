db3567bbb2af4d711c2d370f69fa260f73b552b8
albertus <albertus@psybergate.co.za>
2025-03-08 23:35:32 +0200

    yeet

--- octoprint_filamentdryer/templates/static/js/settings.js
+++ octoprint_filamentdryer/templates/static/js/settings.js
@@ -1,9 +1,7 @@
 $(function() {
     function FilamentDryerViewModel(parameters) {
         var self = this;
-
         self.settings = parameters[0];
-        self.isSystemOn = ko.observable(false);
 
         self.target_temp = ko.observable();
         self.tolerance = ko.observable();
@@ -21,50 +19,20 @@ $(function() {
             self.element_off_cmd(self.settings.settings.plugins.filamentdryer.element_off_cmd());
         };
 
-        self.saveSettings = function() {
+        // Save settings
+        self.onSettingsBeforeSave = function() {
             self.settings.settings.plugins.filamentdryer.target_temp(self.target_temp());
             self.settings.settings.plugins.filamentdryer.tolerance(self.tolerance());
             self.settings.settings.plugins.filamentdryer.fan_on_cmd(self.fan_on_cmd());
             self.settings.settings.plugins.filamentdryer.fan_off_cmd(self.fan_off_cmd());
             self.settings.settings.plugins.filamentdryer.element_on_cmd(self.element_on_cmd());
             self.settings.settings.plugins.filamentdryer.element_off_cmd(self.element_off_cmd());
-
-            self.settings.saveData();
-        };
-
-        self.toggleSystem = function() {
-            self.isSystemOn(!self.isSystemOn());
-
-            $.ajax({
-                url: "/api/plugin/filamentdryer",
-                type: "POST",
-                contentType: "application/json",
-                data: JSON.stringify({ action: self.isSystemOn() ? "start" : "stop" }),
-                success: function(response) {
-                    console.log("System " + (self.isSystemOn() ? "ON" : "OFF"));
-                }
-            });
         };
     }
-
-    $(document).ready(function() {
-        $("#navbar").append(
-            '<li id="filamentdryer-button">' +
-            '<a href="#" id="filamentdryer-toggle">' +
-            '<i class="fas fa-fire"></i>' +
-            '</a>' +
-            '</li>'
-        );
-
-        $("#filamentdryer-toggle").click(function() {
-            var viewModel = ko.dataFor(this);
-            viewModel.toggleSystem();
-        });
-    });
 
     OCTOPRINT_VIEWMODELS.push({
         construct: FilamentDryerViewModel,
         dependencies: ["settingsViewModel"],
-        elements: ["#settings_filamentdryer"]
+        elements: ["#settings_plugin_filamentdryer"]
     });
 });
