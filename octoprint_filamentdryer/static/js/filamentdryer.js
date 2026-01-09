76972a62fbbf545a443f8e57be8a07142b2ffe00
albertus van der walt <albertus@psybergate.co.za>
2026-01-09 20:18:53 +0200

    yeet

--- octoprint_filamentdryer/static/js/filamentdryer.js
+++ octoprint_filamentdryer/static/js/filamentdryer.js
@@ -8,7 +8,6 @@ $(function () {
         };
 
         self.onBeforeBinding = function () {
-            self.loadSettings();
             self.fetchState();
             self.fetchHistory();
             setInterval(self.fetchState, 2000);
@@ -17,11 +16,6 @@ $(function () {
 
         self.fanOn = ko.observable(false);
         self.heaterOn = ko.observable(false);
-        self.fan_pin = ko.observable();
-        self.heater_pin = ko.observable();
-        self.target_temp = ko.observable();
-        self.tolerance = ko.observable();
-        self.saveStatus = ko.observable(false);
         self.actualTempDisplay = ko.observable("--");
         self.humidityDisplay = ko.observable("--");
 
@@ -31,38 +25,6 @@ $(function () {
         // History cache
         self.historyCache = [];
         self.lastTimestamp = 0;
-
-        self.loadSettings = function () {
-            $.get(self.getApiUrl() + "/settings", function (data) {
-                self.fan_pin(data.FAN_PIN);
-                self.heater_pin(data.HEATER_PIN);
-                self.target_temp(data.TARGET_TEMP);
-                self.tolerance(data.TOLERANCE);
-            });
-        };
-
-        self.saveSettings = function () {
-            const settings = {
-                FAN_PIN: parseInt(self.fan_pin()),
-                HEATER_PIN: parseInt(self.heater_pin()),
-                TARGET_TEMP: parseFloat(self.target_temp()),
-                TOLERANCE: parseFloat(self.tolerance())
-            };
-
-            $.ajax({
-                url: self.getApiUrl() + "/settings",
-                method: "POST",
-                contentType: "application/json",
-                data: JSON.stringify(settings),
-                success: function () {
-                    self.saveStatus(true);
-                    setTimeout(() => self.saveStatus(false), 2000);
-                },
-                error: function () {
-                    alert("Failed to save settings.");
-                }
-            });
-        };
 
         self.toggleSystem = function () {
             const newState = !self.systemOn();
